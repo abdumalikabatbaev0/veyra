1a42ef34d36157010b23da229218f07bbfc051bf
abdumalikabatbaev0 <abdumalikabatbaev0@gmail.com>
2026-08-26 14:36:38 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/veyra/tv/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.veyra.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            VeyraApp()
+        }
+    }
+}
+
+@androidx.compose.runtime.Composable
+private fun VeyraApp() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "VEYRA",
+            textAlign = TextAlign.Center
+        )
+    }
+}
