32bc5b6ac605cdddbddec7b0d3d2c7e476d31bb1
abdumalikabatbaev0 <abdumalikabatbaev0@gmail.com>
2026-08-28 11:26:43 +0300

    Update MainActivity.kt

--- app/src/main/java/com/veyra/tv/MainActivity.kt
+++ app/src/main/java/com/veyra/tv/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.tv.material3.Text
 
 class MainActivity : ComponentActivity() {
 
